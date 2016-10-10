4acd62798f2c8d8834be2f7825d1ab26fe76608a
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2016-10-10 21:47:23 +0200

    extending fields with one extra field
    Signed-off-by: Alexander Hofbauer <hofbauer.alexander@gmail.com>

---
+++ fields.php
@@ -0,0 +1,32 @@
+<?php
+function rad_labs_category_colors(){
+    if ( !class_exists( 'RadLabs_Category_Colors' ) )
+        return;
+
+    $meta_sections = array();
+
+    // Color Meta Box
+    $meta_sections[] = array(
+        'title'      => 'Select Category Color',
+        'taxonomies' => array('category'),
+        'id'         => 'rl_category_meta',
+        'fields'     => array(
+            array(
+                'name' => 'Background Color',
+                'id'   => 'rl_cat_color_bg',
+                'type' => 'color',
+            ),
+            array(
+                'name' => 'Foreground Color',
+                'id'   => 'rl_cat_color_fg',
+                'type' => 'color',
+            ),
+        ),
+    );
+
+    foreach ($meta_sections as $meta_section){
+        new RadLabs_Category_Colors( $meta_section );
+    }
+}
+
+add_action( 'admin_init', 'rad_labs_category_colors' );
