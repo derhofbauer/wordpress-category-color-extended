efbc3d9e1900afbdd11876fdc8c84bd190c90619
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2017-08-17 23:13:52 +0000

    Apply fixes from StyleCI

--- src/category_color_extended.php
+++ src/category_color_extended.php
@@ -12,60 +12,68 @@ Text Domain: category-color-extended
 Domain Path: ./i18n/
 */
 
-function categoryColorExtendedLoadTextdomain() {
-    load_plugin_textdomain( 'category-color-extended', FALSE, basename( dirname( __FILE__ ) ) . '/i18n/' );
+function categoryColorExtendedLoadTextdomain()
+{
+    load_plugin_textdomain('category-color-extended', false, basename(dirname(__FILE__)).'/i18n/');
 }
-add_action( 'plugins_loaded', 'categoryColorExtendedLoadTextdomain' );
+add_action('plugins_loaded', 'categoryColorExtendedLoadTextdomain');
 
-class categoryColorsExtended{
+class categoryColorsExtended
+{
     protected $meta;
     protected $taxonomies;
     protected $fields;
 
-    function __construct( $meta ){
-        if ( !is_admin() )
+    public function __construct($meta)
+    {
+        if (!is_admin()) {
             return;
+        }
         $this->meta = $meta;
         $this->normalize();
 
-        add_action( 'admin_init', array( $this, 'add' ), 100 );
-        add_action( 'edit_term', array( $this, 'save' ), 10, 2 );
-        add_action( 'delete_term', array( $this, 'delete' ), 10, 2 );
-        add_action( 'load-edit-tags.php', array( $this, 'loadEditPage' ) );
+        add_action('admin_init', [$this, 'add'], 100);
+        add_action('edit_term', [$this, 'save'], 10, 2);
+        add_action('delete_term', [$this, 'delete'], 10, 2);
+        add_action('load-edit-tags.php', [$this, 'loadEditPage']);
     }
 
     /********************************
      * Enqueue scripts and styles
      ********************************/
-    function loadEditPage(){
+    public function loadEditPage()
+    {
         $screen = get_current_screen();
         if ('edit-tags' != $screen->base
-           || empty( get_query_var('action') )
+           || empty(get_query_var('action'))
            || 'edit' != get_query_var('action')
-           || !in_array( $screen->taxonomy, $this->taxonomies )) {
+           || !in_array($screen->taxonomy, $this->taxonomies)) {
             return;
         }
-        add_action( 'admin_enqueue_scripts', array( $this, 'adminEnqueueScripts' ) );
-        add_action( 'admin_head', array( $this, 'outputCSS' ) );
-        add_action( 'admin_footer', array( $this, 'outputJs' ), 100 );
+        add_action('admin_enqueue_scripts', [$this, 'adminEnqueueScripts']);
+        add_action('admin_head', [$this, 'outputCSS']);
+        add_action('admin_footer', [$this, 'outputJs'], 100);
     }
 
     /*******************************
      * Enqueue scripts and styles
      *******************************/
-    function adminEnqueueScripts(){
-        wp_enqueue_script( 'jquery' );
+    public function adminEnqueueScripts()
+    {
+        wp_enqueue_script('jquery');
         $this->checkFieldColor();
     }
 
     // Output CSS into header
-    function outputCSS(){
-        echo $this->css ? '<style>' . $this->css . '</style>' : '';
+    public function outputCSS()
+    {
+        echo $this->css ? '<style>'.$this->css.'</style>' : '';
     }
 
     // Output JS into footer
-    function outputJs(){
-        echo $this->js ? '<script>jQuery(function($){' . $this->js . '});</script>' : '';
+    public function outputJs()
+    {
+        echo $this->js ? '<script>jQuery(function($){'.$this->js.'});</script>' : '';
     }
 
     /***************
@@ -73,11 +81,13 @@ class categoryColorsExtended{
      ***************/
 
     // Check field color
-    function checkFieldColor(){
-        if ( !$this->hasField( 'color' ) )
+    public function checkFieldColor()
+    {
+        if (!$this->hasField('color')) {
             return;
-        wp_enqueue_style( 'wp-color-picker' );
-        wp_enqueue_script( 'wp-color-picker' );
+        }
+        wp_enqueue_style('wp-color-picker');
+        wp_enqueue_script('wp-color-picker');
         $this->js .= '$(".color").wpColorPicker();';
     }
 
@@ -86,33 +96,39 @@ class categoryColorsExtended{
      *****************/
 
     // Add meta fields for taxonomies
-    function add(){
-        foreach ( get_taxonomies() as $taxName ) {
-            if ( in_array( $taxName, $this->taxonomies ) ) {
-                add_action( $taxName . '_edit_form', array( $this, 'show' ), 9, 2 );
+    public function add()
+    {
+        foreach (get_taxonomies() as $taxName) {
+            if (in_array($taxName, $this->taxonomies)) {
+                add_action($taxName.'_edit_form', [$this, 'show'], 9, 2);
             }
         }
     }
 
     // Show meta fields
-    function show( $tag ){
+    public function show($tag)
+    {
         // get meta fields from option table
-        $metas = get_option( $this->meta['id'] );
-        if ( empty( $metas ) ) $metas = array();
-        if ( !is_array( $metas ) ) $metas = (array) $metas;
+        $metas = get_option($this->meta['id']);
+        if (empty($metas)) {
+            $metas = [];
+        }
+        if (!is_array($metas)) {
+            $metas = (array) $metas;
+        }
 
         // get meta fields for current term
-        $metas = isset( $metas[$tag->termID] ) ? $metas[$tag->termID] : array();
+        $metas = isset($metas[$tag->termID]) ? $metas[$tag->termID] : [];
 
-        wp_nonce_field( basename( __FILE__ ), 'taxonomy_meta_nonce' );
+        wp_nonce_field(basename(__FILE__), 'taxonomy_meta_nonce');
 
         echo "<h3>{$this->meta['title']}</h3><table class='form-table'>";
-        foreach ( $this->fields as $field ) {
+        foreach ($this->fields as $field) {
             echo '<tr>';
 
-            $meta = !empty( $metas[$field['id']] ) ? $metas[$field['id']] : $field['std'];
-            $meta = is_array( $meta ) ? array_map( 'esc_attr', $meta ) : esc_attr( $meta );
-            call_user_func( array( $this, 'show_field_' . $field['type'] ), $field, $meta );
+            $meta = !empty($metas[$field['id']]) ? $metas[$field['id']] : $field['std'];
+            $meta = is_array($meta) ? array_map('esc_attr', $meta) : esc_attr($meta);
+            call_user_func([$this, 'show_field_'.$field['type']], $field, $meta);
 
             echo '</tr>';
         }
@@ -123,95 +139,109 @@ class categoryColorsExtended{
      * META BOX FIELDS
      *******************/
 
-    function showFieldBegin( $field ) {
+    public function showFieldBegin($field)
+    {
         echo "<th scope='row' valign='top'><label for='{$field['id']}'>{$field['name']}</label></th><td>";
     }
 
-    function showFieldEnd( $field ) {
+    public function showFieldEnd($field)
+    {
         echo $field['desc'] ? "<br><span class='description'>{$field['desc']}</span></td>" : '</td>';
     }
 
-    function showFieldColor( $field, $meta ){
-        if ( empty( $meta ) ) $meta = '#';
-        $this->showFieldBegin( $field, $meta );
+    public function showFieldColor($field, $meta)
+    {
+        if (empty($meta)) {
+            $meta = '#';
+        }
+        $this->showFieldBegin($field, $meta);
         echo "<input type='text' name='{$field['id']}' id='{$field['id']}' value='$meta' class='color'>";
-        $this->showFieldEnd( $field, $meta );
+        $this->showFieldEnd($field, $meta);
     }
-
 
     /*****************
      * META BOX SAVE
      *****************/
 
     // Save meta fields
-    function save( $termID ) {
-        $metas = get_option( $this->meta['id'] );
-        if ( !is_array( $metas ) )
+    public function save($termID)
+    {
+        $metas = get_option($this->meta['id']);
+        if (!is_array($metas)) {
             $metas = (array) $metas;
-        $meta = isset( $metas[$termID] ) ? $metas[$termID] : array();
-        foreach ( $this->fields as $field ) {
+        }
+        $meta = isset($metas[$termID]) ? $metas[$termID] : [];
+        foreach ($this->fields as $field) {
             $name = $field['id'];
-            $new = isset( $_POST[$name] ) ? $_POST[$name] : ( $field['multiple'] ? array() : '' );
-            $new = is_array( $new ) ? array_map( 'stripslashes', $new ) : stripslashes( $new );
-            if ( empty( $new ) ) {
-                unset( $meta[$name] );
+            $new = isset($_POST[$name]) ? $_POST[$name] : ($field['multiple'] ? [] : '');
+            $new = is_array($new) ? array_map('stripslashes', $new) : stripslashes($new);
+            if (empty($new)) {
+                unset($meta[$name]);
             } else {
                 $meta[$name] = $new;
             }
         }
         $metas[$termID] = $meta;
-        update_option( $this->meta['id'], $metas );
+        update_option($this->meta['id'], $metas);
     }
 
     /******************
     * META BOX DELETE
     *******************/
 
-    function delete( $termID ){
-        $metas = get_option( $this->meta['id'] );
-        if ( !is_array( $metas ) ) $metas = (array) $metas;
-        unset( $metas[$termID] );
-        update_option( $this->meta['id'], $metas );
+    public function delete($termID)
+    {
+        $metas = get_option($this->meta['id']);
+        if (!is_array($metas)) {
+            $metas = (array) $metas;
+        }
+        unset($metas[$termID]);
+        update_option($this->meta['id'], $metas);
     }
 
     /*********************
      * HELPER FUNCTIONS
      *********************/
 
-    function normalize(){
+    public function normalize()
+    {
         // Default values for meta box
-        $this->meta = array_merge( array(
-            'taxonomies' => array( 'category', 'post_tag' )
-            ), $this->meta );
+        $this->meta = array_merge([
+            'taxonomies' => ['category', 'post_tag'],
+            ], $this->meta);
 
         $this->taxonomies = $this->meta['taxonomies'];
         $this->fields = $this->meta['fields'];
-
     }
 
     // Check if field with $type exists
-    function hasField( $type ) {
-        foreach ( $this->fields as $field ) {
-            if ( $type == $field['type'] ) return true;
+    public function hasField($type)
+    {
+        foreach ($this->fields as $field) {
+            if ($type == $field['type']) {
+                return true;
+            }
         }
+
         return false;
     }
 
-    function getFields( $catid ) {
+    public function getFields($catid)
+    {
         $meta = get_option('category_color_extended_meta');
         if (array_key_exists($catid, $meta)) {
             return $meta[$catid];
         }
-        return "";
+
+        return '';
     }
 }
 
 //Load Taxonomy metaboxes
-require_once('default_fields.php');
+require_once 'default_fields.php';
 
 // provide global function for fetching categories
-function categoryColorExtended($catid){
+function categoryColorExtended($catid)
+{
     return categoryColorsExtended::getFields($catid);
 }
-
-?>
--- src/default_fields.php
+++ src/default_fields.php
@@ -1,42 +1,41 @@
 <?php
-function categoryColorsExtended(){
-    if (class_exists( 'categoryColorsExtended' )) {
 
-        $metaSections = array();
+function categoryColorsExtended()
+{
+    if (class_exists('categoryColorsExtended')) {
+        $metaSections = [];
 
-         // Color Meta Box
-        $metaSections[] = array(
+        // Color Meta Box
+        $metaSections[] = [
             'title'      => __('Select Category Color', 'category-color-extended'),
-            'taxonomies' => array('category'),
+            'taxonomies' => ['category'],
             'id'         => 'category_color_extended_meta',
-            'fields'     => array(
-                array(
+            'fields'     => [
+                [
                     'name' => __('Background Color', 'category-color-extended'),
                     'desc' => __('Color for page background', 'category-color-extended'),
                     'id'   => 'category_color_bg',
                     'type' => 'color',
-                    ),
-                array(
+                    ],
+                [
                     'name' => __('Foreground Color', 'category-color-extended'),
                     'desc' => __('Color for page accents', 'category-color-extended'),
                     'id'   => 'category_color_fg',
                     'type' => 'color',
-                    ),
-                array(
+                    ],
+                [
                     'name' => __('Text On Accent Color', 'category-color-extended'),
                     'desc' => __('Color for text on accents (e. g. button text)', 'category-color-extended'),
                     'id'   => 'category_color_text',
                     'type' => 'color',
-                    ),
-                ),
-            );
+                    ],
+                ],
+            ];
 
-        foreach ($metaSections as $metaSection){
-            new categoryColorsExtended( $metaSection );
+        foreach ($metaSections as $metaSection) {
+            new categoryColorsExtended($metaSection);
         }
     }
 }
 
-add_action( 'admin_init', 'categoryColorsExtended' );
-
-?>
+add_action('admin_init', 'categoryColorsExtended');
